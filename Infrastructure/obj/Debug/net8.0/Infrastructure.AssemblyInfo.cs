d0bb8589dfc194bd8f2b2030633615425c1d7d6d
Shakes <Shakes@DESKTOP-0KCFRMD>
2024-07-20 11:12:36 +0200

    Created CustomerService & ICustomerService

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be602a04eb883a828a9669509136cd1d8fb7a4ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+474cf275ec8c181d007bf277fd51deb934dc5c70")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
