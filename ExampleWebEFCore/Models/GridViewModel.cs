6421b3a246add2f50304e37ca52a5f48b129b567
Shakes <Shakes@DESKTOP-0KCFRMD>
2024-08-09 16:09:57 +0200

    Added Business and Customer Index test data

--- ExampleWebEFCore/Models/GridViewModel.cs
+++ ExampleWebEFCore/Models/GridViewModel.cs
@@ -1,8 +1,9 @@
 ﻿namespace ExampleWebEFCore.Models
 {
-    public class GridViewModel
+    public class GridViewModel<T>
     {
         public string StyleHeader { get; set; }
         public List<string> Columns { get; set; }
+        public List<T> Data { get; set; }
     }
 }
