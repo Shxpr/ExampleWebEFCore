0e7cdfd9101b0a6f7162ffa3943335bb60c2d443
Shakes <Shakes@DESKTOP-0KCFRMD>
2024-09-14 11:51:22 +0200

    New features Customer

---
+++ ExampleWebEFCore/Components/CustomerViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using ExampleWebEFCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleWebEFCore.Components
+{
+    public class CustomerViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(CustomerModel customer)
+        {
+            return View(customer);
+        }
+    }
+}
